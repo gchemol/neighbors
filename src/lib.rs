4a6054154ec955d929ed203e79b9ef40cab92f59
Wenping Guo <ybyygu@gmail.com>
2018-03-19 16:31:01 +0800

    setup rust skeleton

---
+++ src/lib.rs
@@ -0,0 +1,8 @@
+// [[file:~/Workspace/Programming/neighbors/neighbors.note::56b7353e-897b-4eb6-b8a7-89104f8b085f][56b7353e-897b-4eb6-b8a7-89104f8b085f]]
+extern crate cgmath;
+#[macro_use]
+extern crate timeit;
+
+#[macro_use]
+extern crate approx;
+// 56b7353e-897b-4eb6-b8a7-89104f8b085f ends here
